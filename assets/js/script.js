bb5bbf5849176d078e4425554957985c48c28de7
Andres Lopez <andreslpzp@gmail.com>
2024-05-19 21:18:54 -0400

    Fixed desktop and tablet view, only missing the toggle menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-//This code adds a class so that the Toggle menu shows//
-
-const root = document.querySelector('#root')
-const toggler = document.querySelector('#toggler')
-
-toggler.addEventListener('click', ()=> {
-    root.classList.toggle('open')
-})
-
 //This code allows the user to navigate the the different sections using the desktop menu// 
 
 const menuLinks = document.querySelectorAll('.desktop_menu a');
@@ -18,4 +9,13 @@ menuLinks.forEach((link) => {
         const targetSection = document.querySelector(this.getAttribute('href'));
         targetSection.scrollIntoView({ behavior: "smooth" });
     });
+});
+
+//This code shows a lateral menu for the mobile version
+
+const root = document.querySelector('#root');
+const toggler = document.querySelector('#toggler');
+
+toggler.addEventListener('click', () => {
+    root.classList.toggle('open');
 });
